ead32dcc1fa81216ca9d834e0c6f52b7f7067cc5
Chris <electronicsleep@gmail.com>
2018-04-02 19:51:53 -0700

    icinga install

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bahh
+
+apt install icinga2 nagios-plugins monitoring-plugins curl wget
+
+apt install vim-icinga2 vim-addon-manager
+
+apt install mysql-server mysql-client icinga2-ido-mysql
+
+apt install icinga2-studio
+
+vim-addon-manager -w install icinga2
